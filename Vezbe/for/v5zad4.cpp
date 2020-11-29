3b429319fa140b45807b26f1c9fd539340181923
rilex001 <ristic1900@gmail.com>
2020-11-29 22:09:48 +0100

    update

--- Vezbe/for/v5zad4.cpp
+++ Vezbe/for/v5zad4.cpp
@@ -1,38 +1,21 @@
-//#include<stdio.h>
-//int main(){
-//int n,min,max;
-//max = 0;
-//min = 0;
-//
-//for(n = min ; n !=0  ;  ){    
-//	scanf("%d",&n);
-//	if(n < min) {
-//		min=n;
-//	} 
-//    if(n > max) {
-//    	max=n;  	
-//	}     
-//}
-//
-//printf("max=  %d min=%d",max,min);
-//
-//
-//return 0;
-//
-//
-//}
-
-
 #include<stdio.h>
+
+
 int main(){
+	
 int n,min,max;
-max = 0;
+
+scanf("%d",&n);
+max = n;
+min = n;
 
 for(int i = 0 ; n!=0 ;  i++){    
 	scanf("%d",&n);
-	if(n < min) min=n;
-    if(n > max) max=n;      
+	if(n < min) min = n;
+    if(n > max) max = n;      
 }
+
+
 printf("max=%d min=%d",max,min);
 return 0;
 }
